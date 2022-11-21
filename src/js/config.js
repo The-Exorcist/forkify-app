5630f5f87ebec9cc013a823e7f85fa8956a3ca39
The-Exorcist <constantine.main@gmail.com>
2022-11-21 15:51:30 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '9a32bc15-b11c-40e2-845e-2e5bd1111812';
+export const MODAL_CLOSE_SEC = 2.5;
